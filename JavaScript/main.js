320746277dde42e27fef8585bd6f6fcc4e67dc4a
Ian Almeida <iangtaonline1@gmail.com>
2022-09-21 10:30:20 -0300

    calculando imc do primeiro paciente

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,15 +1,16 @@
 let titulo = document.querySelector('.titulo');
 titulo.textContent = 'Aparecida Nutricionista'
-
+//buscando paciente
 let paciente = document.querySelector('#primeiro-paciente')
-
+// pegando o peso 
 let tdpeso = paciente.querySelector('.info-peso')
 let peso = tdpeso.textContent
-
+// Pegando altura
 let tdaltura = paciente.querySelector('.info-altura')
 let altura = tdaltura.textContent
-
+//calculando imc
 const imc = peso / (altura * altura)
+//substituindo o imc na tabela
+let imcInfo = paciente.querySelector('.info-imc')
+imcInfo.textContent = imc
 
-
-console.log(imc)

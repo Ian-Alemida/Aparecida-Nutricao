dedd90e36b7755d1f2e0c0073afb5d380deea766
Ian Almeida <iangtaonline1@gmail.com>
2022-09-21 10:25:31 -0300

    calculando imc

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,3 +1,15 @@
-let titulo = document.querySelector('h1');
-
+let titulo = document.querySelector('.titulo');
 titulo.textContent = 'Aparecida Nutricionista'
+
+let paciente = document.querySelector('#primeiro-paciente')
+
+let tdpeso = paciente.querySelector('.info-peso')
+let peso = tdpeso.textContent
+
+let tdaltura = paciente.querySelector('.info-altura')
+let altura = tdaltura.textContent
+
+const imc = peso / (altura * altura)
+
+
+console.log(imc)

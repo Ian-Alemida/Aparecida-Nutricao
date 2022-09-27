c25dda43a4ccb8c7cffd29f4210b7c7ca1c0c5c8
Ian Almeida <iangtaonline1@gmail.com>
2022-09-27 14:09:13 -0300

    removendo classe invisivel caso o input esteja vazio

--- JavaScript/filtraPacientes.js
+++ JavaScript/filtraPacientes.js
@@ -1,1 +1,26 @@
 const input = document.querySelector('#filtra_paciente')
+
+input.addEventListener('input', function(){
+
+    var pacientes= document.querySelectorAll('.paciente');
+
+    if(this.value.length > 0){
+        for( i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdnome = paciente.querySelector('.info-nome');
+            var nome = tdnome.textContent;
+    
+            if (nome != this.value){
+                paciente.classList.add('invisivel')
+            }else{
+                paciente.classList.remove('invisivel')
+            }
+        }
+    }else{
+        for(i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i]
+            paciente.classList.remove('invisivel')
+        }
+    }
+
+})

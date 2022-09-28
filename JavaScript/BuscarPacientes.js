f0ba5a371dff10b18a1be9d086f5a8d9d5aa68d5
Ian Almeida <iangtaonline1@gmail.com>
2022-09-28 18:18:56 -0300

    verificando se o XML foi encontrado

--- JavaScript/BuscarPacientes.js
+++ JavaScript/BuscarPacientes.js
@@ -1,20 +1,27 @@
 const botaoAdicionar = document.querySelector('#buscar-Pacientes')
 
 botaoAdicionar.addEventListener('click', function(){
-    
+
     let xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes32");
 
     xhr.addEventListener("load", function(){
-        var resposta = xhr.responseText
-        var pacientesArray = JSON.parse(resposta)
 
-        pacientesArray.forEach(function(paciente){
+        var erroAJAX = document.querySelector('#erro-ajax')
+
+        if(xhr.status == 200){
+            erroAJAX.classList.add('invisivel')
+            var resposta = xhr.responseText
+            var pacientesArray = JSON.parse(resposta)
+
+            pacientesArray.forEach(function(paciente){
             adicionaPacienteNaTabela(paciente)
-        })
-
-    })
+            });
+        }else{
+            erroAJAX.classList.remove('invisivel')
+        };
+    });
 
     xhr.send();
 })

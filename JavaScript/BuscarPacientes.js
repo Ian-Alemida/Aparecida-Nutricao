b022bd6c8069be65058739f45fae0402f4a7a6bf
Ian Almeida <iangtaonline1@gmail.com>
2022-09-28 15:16:19 -0300

    buscando pacientes de outra pagina e os transformando em objetos js

---
+++ JavaScript/BuscarPacientes.js
@@ -0,0 +1,23 @@
+const botaoAdicionar = document.querySelector('#buscar-Pacientes')
+
+botaoAdicionar.addEventListener('click', function(){
+    console.log('buscando...');
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load", function(){
+        var resposta = xhr.responseText
+        var pacientesArray = JSON.parse(resposta)
+
+        pacientesArray.forEach(function(paciente){
+            obtemDadosDoFormulario(paciente)
+        })
+
+        console.log(pacientesArray)
+        console.log(typeof pacientesArray);
+    })
+
+    xhr.send();
+})
